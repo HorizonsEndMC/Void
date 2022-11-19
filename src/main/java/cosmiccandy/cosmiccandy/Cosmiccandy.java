f35c26bd7f8d672fcae3c305e24c0b5e17c18cdb
Sciath <67932943+SciathGH@users.noreply.github.com>
2022-11-19 19:15:51 +0000

    Void

---
+++ src/main/java/cosmiccandy/cosmiccandy/Cosmiccandy.java
@@ -0,0 +1,9 @@
+package cosmiccandy.cosmiccandy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cosmiccandy implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/cosmiccandy/cosmiccandy/client/mixins/WorldRendererMixin.java
@@ -0,0 +1,18 @@
+//package cosmiccandy.cosmiccandy.client.mixins;
+
+//import net.minecraft.client.render.RenderLayer;
+//import net.minecraft.client.render.WorldRenderer;
+//import net.minecraft.client.util.math.MatrixStack;
+//import net.minecraft.util.math.Matrix4f;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+//@Mixin(WorldRenderer.class)
+//public class WorldRendererMixin {
+//    @Inject(method = "renderLayer", at = @At("HEAD"))
+//    private void onRenderLayer(RenderLayer renderLayer, MatrixStack matrixStack, double x, double y, double z, Matrix4f matrix4f, CallbackInfo ci){
+
+//    }
+//}
